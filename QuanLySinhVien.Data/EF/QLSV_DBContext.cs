b749514db8cd572dc500db2339ed4d65d3f704a2
DaoCuong <cuong.263@gmail.com>
2020-08-13 13:12:22 +0700

    Create solution structure

---
+++ QuanLySinhVien.Data/EF/QLSV_DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien.Data.EF
+{
+    public class QLSV_DBContext : DbContext
+    {
+        public QLSV_DBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
