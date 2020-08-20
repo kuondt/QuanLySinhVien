f125d4945fe9f0181a2f09deac9d5074b07efa66
DaoCuong <cuong.263@gmail.com>
2020-08-20 13:41:59 +0700

    Add user login view model & login request

---
+++ QuanLySinhVien.Application/System/Users/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using QuanLySinhVien.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.Service.System.Users
+{
+    public interface IUserService
+    {
+        Task<ApiResult<string>> Authencate(LoginRequest request);
+
+        Task<ApiResult<bool>> Register(RegisterRequest request);
+
+        Task<ApiResult<bool>> Update(Guid id, UserUpdateRequest request);
+
+        Task<ApiResult<PagedResult<UserViewModel>>> GetUserPaging(GetUserPagingRequest request);
+
+        Task<ApiResult<UserViewModel>> GetById(Guid id);
+
+        Task<ApiResult<bool>> Delete(Guid id);
+
+        Task<ApiResult<bool>> RoleAssign(Guid id, RoleAssignRequest request);
+    }
+}
