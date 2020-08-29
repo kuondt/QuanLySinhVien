c8b6a73b8972e81de3554a84337f4f709c897d8b
DaoCuong <cuong.263@gmail.com>
2020-08-29 20:50:28 +0700

    Lop Bien Che view model

---
+++ QuanLySinhVien.ViewModel/Catalog/LopBienChe/LopBienCheUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien.ViewModel.Catalog.LopBienChe
+{
+    class LopBienCheUpdateRequest
+    {
+    }
+}
