5529fc2bb54f801c641bd8ce0157932a8bbd2a3b
DaoCuong <cuong.263@gmail.com>
2020-08-30 12:15:36 +0700

    Lop bien che index page

--- QuanLySinhVien.AdminApp/Controllers/LopBienCheController.cs
+++ QuanLySinhVien.AdminApp/Controllers/LopBienCheController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using QuanLySinhVien.AdminApp.Services.LopBienChe;
+using QuanLySinhVien.ViewModel.Catalog.LopBienChe;
 
 namespace QuanLySinhVien.AdminApp.Controllers
 {
-    public class LopBienCheController : Controller
+    public class LopBienCheController : BaseController
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration;
+        private readonly ILopBienCheApiClient _lopBienCheApiClient;
+
+        public LopBienCheController(IConfiguration configuration, ILopBienCheApiClient lopBienCheApiClient)
         {
-            return View();
+            _lopBienCheApiClient = lopBienCheApiClient;
+            _configuration = configuration;
         }
+
+        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 3)
+        {
+            var request = new LopBienCheManagePagingRequest()
+            {
+                Keyword = keyword,
+                PageIndex = pageIndex,
+                PageSize = pageSize
+            };
+            var data = await _lopBienCheApiClient.GetAllPaging(request);
+
+            ViewBag.Keyword = keyword;
+
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMessage = TempData["result"];
+            }
+
+            return View(data);
+        }
+
     }
 }
--- QuanLySinhVien.AdminApp/Services/LopBienChe/LopBienCheApiClient.cs
+++ QuanLySinhVien.AdminApp/Services/LopBienChe/LopBienCheApiClient.cs
@@ -1,14 +1,31 @@
-﻿using QuanLySinhVien.ViewModel.Catalog.LopBienChe;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using QuanLySinhVien.ViewModel.Catalog.LopBienChe;
 using QuanLySinhVien.ViewModel.Common;
+using QuanLySinhVien.ViewModel.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace QuanLySinhVien.AdminApp.Services.LopBienChe
 {
     public class LopBienCheApiClient : ILopBienCheApiClient
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LopBienCheApiClient(IHttpClientFactory httpClientFactory, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        {
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _httpClientFactory = httpClientFactory;
+        }
+
         public Task<bool> Create(LopBienCheCreateRequest request)
         {
             throw new NotImplementedException();
@@ -19,9 +36,27 @@ namespace QuanLySinhVien.AdminApp.Services.LopBienChe
             throw new NotImplementedException();
         }
 
-        public Task<PagedResult<LopBienCheViewModel>> GetAllPaging(LopBienCheManagePagingRequest request)
+        public async Task<PagedResult<LopBienCheViewModel>> GetAllPaging(LopBienCheManagePagingRequest request)
         {
-            throw new NotImplementedException();
+            var sessions = _httpContextAccessor
+                            .HttpContext
+                            .Session
+                            .GetString(SystemConstants.AppSettings.Token);
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration[SystemConstants.AppSettings.BaseAddress]);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+
+            var response = await client.GetAsync(
+                $"/api/lopbienches/paging?pageIndex={request.PageIndex}" +
+                $"&pageSize={request.PageSize}" +
+                $"&keyword={request.Keyword}"
+                );
+
+            var body = await response.Content.ReadAsStringAsync();
+            var lopBienChe = JsonConvert.DeserializeObject<PagedResult<LopBienCheViewModel>>(body);
+
+            return lopBienChe;
         }
 
         public Task<LopBienCheViewModel> GetById(string id)
