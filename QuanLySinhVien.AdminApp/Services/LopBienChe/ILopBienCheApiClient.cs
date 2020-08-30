9374d41fd287d10e5b2eb4209afcd40f29d5d5ca
DaoCuong <cuong.263@gmail.com>
2020-08-30 11:36:22 +0700

    Lop bien che interface

--- QuanLySinhVien.AdminApp/Services/LopBienChe/ILopBienCheApiClient.cs
+++ QuanLySinhVien.AdminApp/Services/LopBienChe/ILopBienCheApiClient.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using QuanLySinhVien.ViewModel.Catalog.LopBienChe;
+using QuanLySinhVien.ViewModel.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QuanLySinhVien.AdminApp.Services.LopBienChe
 {
-    interface ILopBienCheApiClient
+    public interface ILopBienCheApiClient
     {
+        Task<bool> Create(LopBienCheCreateRequest request);
+
+        Task<LopBienCheViewModel> GetById(string id);
+
+        Task<bool> Update(string id, LopBienCheUpdateRequest request);
+
+        Task<PagedResult<LopBienCheViewModel>> GetAllPaging(LopBienCheManagePagingRequest request);
+
+        Task<bool> Delete(string id);
     }
 }
