78c2187aad28df90433511edd8954ea49f6d65dd
DaoCuong <cuong.263@gmail.com>
2020-08-31 14:17:57 +0700

    Add api client for sinh vien interface

---
+++ QuanLySinhVien.AdminApp/Services/SinhVien/ISinhVienApiClient.cs
@@ -0,0 +1,20 @@
+﻿using QuanLySinhVien.ViewModel.Catalog.SinhViens;
+using QuanLySinhVien.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.AdminApp.Services.SinhVien
+{
+    public interface ISinhVienApiClient
+    {
+        Task<bool> Create(SinhVienCreateRequest request);
+
+        Task<SinhVienViewModel> GetById(string id);
+
+        Task<bool> Update(string id, SinhVienUpdateRequest request);
+
+        Task<PagedResult<SinhVienViewModel>> GetAllPaging(SinhVienManagePagingRequest request);
+    }
+}
