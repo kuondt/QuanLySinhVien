a0a6ecc650f343515d48d8e10ef17fd8ea5016a6
DaoCuong <cuong.263@gmail.com>
2020-09-02 14:52:30 +0700

    Phong service create

--- QuanLySinhVien.Application/Catalog/Phongs/IPhongService.cs
+++ QuanLySinhVien.Application/Catalog/Phongs/IPhongService.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using QuanLySinhVien.ViewModel.Catalog.Phongs;
+using QuanLySinhVien.ViewModel.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace QuanLySinhVien.Service.Catalog.Phongs
+namespace QuanLyPhong.Service.Catalog.Phongs
 {
     public interface IPhongService
     {
+        Task<string> Create(PhongCreateRequest request);
+
+        Task<int> Update(string id, PhongUpdateRequest request);
+
+        Task<PhongViewModel> GetById(string id);
+
+        Task<PagedResult<PhongViewModel>> GetAllPaging(PhongManagePagingRequest request);
     }
 }
--- QuanLySinhVien.Application/Catalog/Phongs/PhongService.cs
+++ QuanLySinhVien.Application/Catalog/Phongs/PhongService.cs
@@ -1,10 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using QuanLySinhVien.ViewModel.Catalog.Phongs;
+using QuanLySinhVien.Data.EF;
+using System.Linq;
+using QuanLySinhVien.Data.Entities;
+using QuanLySinhVien.ViewModel.Common;
 
-namespace QuanLySinhVien.Service.Catalog.Phongs
+namespace QuanLyPhong.Service.Catalog.Phongs
 {
-    public class PhongService
+    public class PhongService : IPhongService
     {
+        private readonly QLSV_DBContext _context;
+
+        public PhongService(QLSV_DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string> Create(PhongCreateRequest request)
+        {
+            //STT mặc định là 1
+            //STT = số thứ tự cuối cùng của năm đó + 1
+            int soThuTu_Phong = 1;
+            var sttCuoiCung_Phong_CuaNam = _context.Phongs
+                                                .Select(x => x.SoThuTu)
+                                                .ToArray()
+                                                .LastOrDefault();
+            soThuTu_Phong += sttCuoiCung_Phong_CuaNam;
+
+
+            //Ghép chuỗi tạo ID
+            string ID_Phong =  "PH" + soThuTu_Phong.ToString().PadLeft(3, '0');
+
+            var phong = new Phong()
+            {
+                ID = ID_Phong,
+                SoThuTu = soThuTu_Phong,
+                TenCoSo = request.TenCoSo
+            };
+
+            _context.Phongs.Add(phong);
+            await _context.SaveChangesAsync();
+
+            return phong.ID;
+        }
+
+        public Task<PagedResult<PhongViewModel>> GetAllPaging(PhongManagePagingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PhongViewModel> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(string id, PhongUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
