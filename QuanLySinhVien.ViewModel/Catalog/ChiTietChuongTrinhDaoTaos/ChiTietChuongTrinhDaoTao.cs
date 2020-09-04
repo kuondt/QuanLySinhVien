7af86bf527cc8a6a63b335167a763a6dc2aaa720
DaoCuong <cuong.263@gmail.com>
2020-09-04 16:20:24 +0700

    Add chi tiet CTDT view model

---
+++ QuanLySinhVien.ViewModel/Catalog/ChiTietChuongTrinhDaoTaos/ChiTietChuongTrinhDaoTao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuanLySinhVien.Data.Entities;
+
+namespace QuanLySinhVien.ViewModel.Catalog.ChiTietChuongTrinhDaoTaos
+{
+    public class ChiTietChuongTrinhDaoTao
+    {
+        public string ID_ChuongTrinhDaoTao { get; set; }
+        public int HK_HocKy { get; set; }
+        public int HK_NamHoc { get; set; }
+        public string ID_MonHoc { get; set; }
+        public ChuongTrinhDaoTao ChuongTrinhDaoTao { get; set; }
+        public HocKy_NamHoc HocKy_NamHoc { get; set; }
+        public MonHoc MonHoc { get; set; }
+    }
+}
---
+++ QuanLySinhVien.ViewModel/Catalog/ChiTietChuongTrinhDaoTaos/ChiTietChuongTrinhDaoTaoPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuanLySinhVien.ViewModel.Common;
+
+namespace QuanLySinhVien.ViewModel.Catalog.ChiTietChuongTrinhDaoTaos
+{
+    public class ChiTietChuongTrinhDaoTaoPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+    }
+}
