132726d153262576827bb6f740e585ff4ac67086
DaoCuong <cuong.263@gmail.com>
2020-09-04 16:17:29 +0700

    Add chuong trinh dao tao view model

---
+++ QuanLySinhVien.ViewModel/Catalog/ChuongTrinhDaoTaos/ChuongTrinhDaoTaoPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuanLySinhVien.ViewModel.Common;
+
+namespace QuanLySinhVien.ViewModel.Catalog.ChuongTrinhDaoTaos
+{
+    public class ChuongTrinhDaoTaoPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+    }
+}
---
+++ QuanLySinhVien.ViewModel/Catalog/ChuongTrinhDaoTaos/ChuongTrinhDaoTaoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuanLySinhVien.Data.Entities;
+
+namespace QuanLySinhVien.ViewModel.Catalog.ChuongTrinhDaoTaos
+{
+    public class ChuongTrinhDaoTaoViewModel
+    {
+        public string ID { get; set; }
+        public string TenChuongTrinh { get; set; }
+        public int Nam { get; set; }
+        public string Id_Khoa { get; set; }
+        public Khoa Khoa { get; set; }
+        public List<ChiTiet_ChuongTrinhDaoTao_MonHoc> ChiTiet_ChuongTrinhDaoTao_MonHocs { get; set; }
+    }
+}
