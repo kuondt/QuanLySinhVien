e6c7b1584ecb4dc25ca0915bc9727c7ea985977c
DaoCuong <cuong.263@gmail.com>
2020-09-05 16:50:42 +0700

    Chi tiet CTDT inplement interface

--- QuanLySinhVien.AdminApp/Services/ChiTietChuongTrinhDaoTao/ChiTietChuongTrinhDaoTaoApiClient.cs
+++ QuanLySinhVien.AdminApp/Services/ChiTietChuongTrinhDaoTao/ChiTietChuongTrinhDaoTaoApiClient.cs
@@ -1,32 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using QuanLySinhVien.ViewModel.Catalog.ChiTietChuongTrinhDaoTaos;
 using QuanLySinhVien.ViewModel.Common;
+using QuanLySinhVien.ViewModel.Constants;
 
 namespace QuanLySinhVien.AdminApp.Services.ChiTietChuongTrinhDaoTao
 {
     public class ChiTietChuongTrinhDaoTaoApiClient : IChiTietChuongTrinhDaoTaoApiClient
     {
-        public Task<bool> Create(ChiTietChuongTrinhDaoTaoCreateRequest request)
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ChiTietChuongTrinhDaoTaoApiClient(IHttpClientFactory httpClientFactory, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public Task<PagedResult<ChiTietChuongTrinhDaoTaoViewModel>> GetAllPaging(ChiTietChuongTrinhDaoTaoPagingRequest request)
+        public async Task<bool> Create(ChiTietChuongTrinhDaoTaoCreateRequest request)
         {
-            throw new NotImplementedException();
+            var sessions = _httpContextAccessor
+                            .HttpContext
+                            .Session
+                            .GetString(SystemConstants.AppSettings.Token);
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration[SystemConstants.AppSettings.BaseAddress]);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+
+            var jsonString = JsonConvert.SerializeObject(request);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync($"/api/chitietchuongtrinhdaotaos/", content);
+
+            return response.IsSuccessStatusCode;
         }
 
-        public Task<ChiTietChuongTrinhDaoTaoViewModel> GetById(string id_CTDT, string id_MonHoc, int hocKy, int namHoc)
+        public async Task<PagedResult<ChiTietChuongTrinhDaoTaoViewModel>> GetAllPaging(ChiTietChuongTrinhDaoTaoPagingRequest request)
         {
-            throw new NotImplementedException();
+            var sessions = _httpContextAccessor
+                             .HttpContext
+                             .Session
+                             .GetString(SystemConstants.AppSettings.Token);
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration[SystemConstants.AppSettings.BaseAddress]);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+
+            var response = await client.GetAsync(
+                $"/api/hockynamhocs/paging?pageIndex={request.PageIndex}" +
+                $"&pageSize={request.PageSize}" +
+                $"&keyword={request.Keyword}"
+                );
+
+            var body = await response.Content.ReadAsStringAsync();
+            var chiTiet_CTDT = JsonConvert.DeserializeObject<PagedResult<ChiTietChuongTrinhDaoTaoViewModel>>(body);
+
+            return chiTiet_CTDT;
         }
 
-        public Task<bool> Update(string id_CTDT, string id_MonHoc, int hocKy, int namHoc, ChiTietChuongTrinhDaoTaoUpdateRequest request)
+        public async Task<ChiTietChuongTrinhDaoTaoViewModel> GetById(string id_CTDT, string id_MonHoc, int hocKy, int namHoc)
         {
-            throw new NotImplementedException();
+            var sessions = _httpContextAccessor
+                            .HttpContext
+                            .Session
+                            .GetString(SystemConstants.AppSettings.Token);
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration[SystemConstants.AppSettings.BaseAddress]);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+
+            var response = await client.GetAsync(
+                $"/api/hockynamhocs/{id_CTDT}/{id_MonHoc}/{hocKy}/{namHoc}"
+                );
+
+            var body = await response.Content.ReadAsStringAsync();
+            var chiTiet_CTDT = JsonConvert.DeserializeObject<ChiTietChuongTrinhDaoTaoViewModel>(body);
+
+            return chiTiet_CTDT;
+        }
+
+        public async Task<bool> Update(string id_CTDT, string id_MonHoc, int hocKy, int namHoc, ChiTietChuongTrinhDaoTaoUpdateRequest request)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration["BaseAddress"]);
+            var sessions = _httpContextAccessor.HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessions);
+
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsync($"/api/hockynamhocs/{id_CTDT}/{id_MonHoc}/{hocKy}/{namHoc}", httpContent);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
