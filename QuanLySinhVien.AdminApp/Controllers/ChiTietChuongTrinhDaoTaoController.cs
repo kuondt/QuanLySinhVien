5442ef4523f886386ed16c29024ba3d94dc24664
DaoCuong <cuong.263@gmail.com>
2020-09-05 16:32:49 +0700

    Add file for Chi tiet CTDT in admin app

---
+++ QuanLySinhVien.AdminApp/Controllers/ChiTietChuongTrinhDaoTaoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLySinhVien.AdminApp.Controllers
+{
+    public class ChiTietChuongTrinhDaoTaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
