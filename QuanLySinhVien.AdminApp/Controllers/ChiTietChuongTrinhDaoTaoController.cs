b1501c94a3d23afb40089c5e498d65a61f026f3f
DaoCuong <cuong.263@gmail.com>
2020-09-05 16:36:23 +0700

    Chi tiet CTDT interface

--- QuanLySinhVien.AdminApp/Controllers/ChiTietChuongTrinhDaoTaoController.cs
+++ QuanLySinhVien.AdminApp/Controllers/ChiTietChuongTrinhDaoTaoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace QuanLySinhVien.AdminApp.Controllers
+namespace QuanLySinhVien.AdminApp.Controllers.Components
 {
     public class ChiTietChuongTrinhDaoTaoController : Controller
     {
--- QuanLySinhVien.AdminApp/Services/ChiTietChuongTrinhDaoTao/IChiTietChuongTrinhDaoTaoApiService.cs
+++ QuanLySinhVien.AdminApp/Services/ChiTietChuongTrinhDaoTao/IChiTietChuongTrinhDaoTaoApiService.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using QuanLySinhVien.ViewModel.Catalog.ChiTietChuongTrinhDaoTaos;
+using QuanLySinhVien.ViewModel.Common;
 
 namespace QuanLySinhVien.AdminApp.Services.ChiTietChuongTrinhDaoTao
 {
     public interface IChiTietChuongTrinhDaoTaoApiService
     {
+        Task<bool> Create(ChiTietChuongTrinhDaoTaoCreateRequest request);
+
+        Task<ChiTietChuongTrinhDaoTaoViewModel> GetById(int hocky, int namhoc);
+
+        Task<bool> Update(int hocky, int namhoc, ChiTietChuongTrinhDaoTaoUpdateRequest request);
+
+        Task<PagedResult<ChiTietChuongTrinhDaoTaoViewModel>> GetAllPaging(ChiTietChuongTrinhDaoTaoPagingRequest request);
     }
 }
