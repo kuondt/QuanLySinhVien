adfbf3664ba95b532ed229675e076e08a419486a
DaoCuong <cuong.263@gmail.com>
2020-09-08 22:58:29 +0700

    Change chi tiet CTDT model

--- QuanLySinhVien.ViewModel/Catalog/ChiTietChuongTrinhDaoTaos/ChiTietChuongTrinhDaoTaoViewModel.cs
+++ QuanLySinhVien.ViewModel/Catalog/ChiTietChuongTrinhDaoTaos/ChiTietChuongTrinhDaoTaoViewModel.cs
@@ -9,8 +9,8 @@ namespace QuanLySinhVien.ViewModel.Catalog.ChiTietChuongTrinhDaoTaos
     {
         public string ID_ChuongTrinhDaoTao { get; set; }
         public int HK_HocKy { get; set; }
-        public int HK_NamHoc { get; set; }
-        public string ID_MonHoc { get; set; }
+        public int HocKyDuKien { get; set; }
+        public int Nam { get; set; }
         public ChuongTrinhDaoTao ChuongTrinhDaoTao { get; set; }
         public HocKy_NamHoc HocKy_NamHoc { get; set; }
         public MonHoc MonHoc { get; set; }
