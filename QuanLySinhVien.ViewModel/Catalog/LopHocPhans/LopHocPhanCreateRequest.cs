d6615ec4b8391f2770c63d753309f0aa16d33a63
DaoCuong <cuong.263@gmail.com>
2020-09-11 22:32:52 +0700

    Add lop hoc phan view model

---
+++ QuanLySinhVien.ViewModel/Catalog/LopHocPhans/LopHocPhanCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien.ViewModel.Catalog.LopHocPhans
+{
+    class LopHocPhanCreateRequest
+    {
+    }
+}
