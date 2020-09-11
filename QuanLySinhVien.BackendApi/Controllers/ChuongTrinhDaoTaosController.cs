3faa49f36c48f9fa61f1c2d81d5e6e873b29454d
DaoCuong <cuong.263@gmail.com>
2020-09-11 22:55:31 +0700

    Add Authorize for lop bien che

--- QuanLySinhVien.BackendApi/Controllers/ChuongTrinhDaoTaosController.cs
+++ QuanLySinhVien.BackendApi/Controllers/ChuongTrinhDaoTaosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuanLySinhVien.Service.Catalog.ChuongTrinhDaoTaos;
@@ -11,6 +12,7 @@ namespace QuanLySinhVien.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ChuongTrinhDaoTaosController : ControllerBase
     {
         private readonly IChuongTrinhDaoTaoService _chuongTrinhDaoTaoService;
