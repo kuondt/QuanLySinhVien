f6d886090c11f2289e89afdb1c6ff5a336798108
DaoCuong <cuong.263@gmail.com>
2020-09-15 23:12:48 +0700

    Lop Hoc Phan client service

---
+++ QuanLySinhVien.AdminApp/Services/LopHocPhan/ILopHocPhanApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuanLySinhVien.ViewModel.Catalog.LopHocPhans;
+using QuanLySinhVien.ViewModel.Common;
+
+namespace QuanLySinhVien.AdminApp.Services.LopHocPhan
+{
+    public interface ILopHocPhanApiClient
+    {
+        Task<bool> Create(LopHocPhanCreateRequest request);
+
+        Task<bool> Update(string id, LopHocPhanUpdateRequest request);
+
+        Task<LopHocPhanViewModel> GetById(string id);
+
+        Task<PagedResult<LopHocPhanViewModel>> GetAllPaging(LopHocPhanManagePagingRequest request);
+
+        Task<bool> Delete(string id);
+
+        Task<PagedResult<LopHocPhanViewModel>> GetSchedule(int hocky, int namhoc);
+
+        Task<bool> Schedule(int hocky, int namhoc, ScheduleCreateRequest request);
+    }
+}
