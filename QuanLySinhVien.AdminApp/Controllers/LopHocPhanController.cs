cf117cb4423f1b4ca90ff950fa17a5d78fcdc5e1
DaoCuong <cuong.263@gmail.com>
2020-09-16 22:41:12 +0700

    Add lop hoc phan controller file

---
+++ QuanLySinhVien.AdminApp/Controllers/LopHocPhanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using QuanLySinhVien.AdminApp.Services.LopHocPhan;
+
+namespace QuanLySinhVien.AdminApp.Controllers
+{
+    public class LopHocPhanController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILopHocPhanApiClient _lopHocPhanApiClient;
+
+        public LopHocPhanController(IConfiguration configuration, ILopHocPhanApiClient lopHocPhanApiClient)
+        {
+            _configuration = configuration;           
+            _lopHocPhanApiClient = lopHocPhanApiClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
