2e34c7ad7f88c7b5b1ac88d1ccf88e9e4e572cc4
DaoCuong <cuong.263@gmail.com>
2020-10-02 22:55:49 +0700

    Add file for Danh sach sinh vien lop hoc phan view model

--- QuanLySinhVien.Application/Catalog/DanhSachSinhViens/IDanhSachSinhVienService.cs
+++ QuanLySinhVien.Application/Catalog/DanhSachSinhViens/IDanhSachSinhVienService.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace QuanLySinhVien.Service.Catalog.DanhSachSinhViens
 {
     public interface IDanhSachSinhVienService
     {
+        Task<Tuple<string, string>> Create(DanhSachSinhVienCreateRequest request);
+
+        Task<ChiTietChuongTrinhDaoTaoViewModel> GetById(string id_CTDT, string id_MonHoc);
+
+        Task<PagedResult<ChiTietChuongTrinhDaoTaoViewModel>> GetAll(ChiTietChuongTrinhDaoTaoPagingRequest request);
+
+        Task<int> Delete(string id_CTDT, string id_MonHoc);
     }
 }
---
+++ QuanLySinhVien.ViewModel/Catalog/DanhSachSinhViens/DanhSachSinhVienUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien.ViewModel.Catalog.DanhSachSinhViens
+{
+    public class DanhSachSinhVienUpdateRequest
+    {
+    }
+}
